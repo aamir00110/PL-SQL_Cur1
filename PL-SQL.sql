cd4fef497eef227ebc7c5c5cdac75bd0e990a711
Aamir shehzad <aamirs13370@gmail.com>
2026-05-14 10:29:12 +0500

    Update PL-SQL.sql

--- PL-SQL.sql
+++ PL-SQL.sql
@@ -1,1 +1,26 @@
-
+DECLARE
+    CURSOR cur_name IS
+        SELECT column1,
+               column2,
+               column3
+        FROM your_table;
+BEGIN
+    FOR rec IN cur_name LOOP
+        INSERT INTO table_name
+        (
+            column1,
+            column2,
+            column3
+        )
+        VALUES
+        (
+            rec.column1,
+            rec.column2,
+            rec.column3
+        );
+    END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END;
+/
